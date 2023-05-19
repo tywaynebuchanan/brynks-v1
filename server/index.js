0d78f745f7570facfa322245957d24b03e2a97bc
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2023-05-19 10:46:22 -0500

    config vercel.json

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ DB.once("open", () => {
 app.use(morgan("tiny"))
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://brynks-v1.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
