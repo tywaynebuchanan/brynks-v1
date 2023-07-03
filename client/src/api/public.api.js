39538702bd1c944e9f98a4c12e2a21d3c5b01e74
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2023-07-03 12:04:29 -0500

    made changes to package.json

--- client/src/api/public.api.js
+++ client/src/api/public.api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const publicClient = axios.create({
-    // baseURL:"http://localhost:5000",
-    baseURL: "https://brynks-api.onrender.com"
+    baseURL:"http://localhost:5000",
+    // baseURL: "https://brynks-api.onrender.com"
   
 })
 
--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -82,7 +82,7 @@ exports.login = async (req, res, next) => {
 
 		const token = createToken(user._id)
 
-		res.cookie("jwt", token, {httpOnly: true, maxAge: maxAge * 1000})
+		res.cookie("jwt", token, {httpOnly: false, maxAge: maxAge * 1000})
 
 		res.status(200).json({
 			user: user._id,
